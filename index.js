ac0f99c82924f1ed9ca8094cef312565996df3c6
Mehmood Ul Hassan <167098807+MehmoodulHasssan@users.noreply.github.com>
2024-08-26 14:50:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ const PORT = process.env.PORT || 8080;
 //Middlewares
 app.use(
   cors({
-    origin: '*'
-      // ['https://food-order-pgo4xthc8-mehmoodulhasssans-projects.vercel.app'],  Your frontend URL
-      
+    origin: 'https://food-order-pgo4xthc8-mehmoodulhasssans-projects.vercel.app', // Your frontend URL
     credentials: true, // Allows cookies to be sent
+    methods: 'GET,POST,PATCH,DELETE,OPTIONS', // Allowed methods
+    allowedHeaders: 'Content-Type,Authorization', // Allowed headers
   })
 );
-app.options('*', cors())
+
+app.options('*', cors()); // Preflight request handling
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
