41f098a9f9755a4926febf6ccef0aabe39dbe624
Mehmood Ul Hassan <167098807+MehmoodulHasssan@users.noreply.github.com>
2024-08-26 14:43:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ app.use(cookieParser());
 
 app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   // res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 //connectDatabase
 connectDb();
 
