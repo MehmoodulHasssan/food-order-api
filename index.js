80e6c25f110bc8846efbaf52366b2ba72fb38985
Mehmood Ul Hassan <167098807+MehmoodulHasssan@users.noreply.github.com>
2024-09-02 18:28:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const PORT = process.env.PORT || 8080;
 // console.log(process.memoryUsage());
 //Middlewares
 
-// app.use(cors({
-//   origin: 'https://food-order-khaki.vercel.app', // Frontend URL
-//   credentials: true, // Allow credentials like cookies
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allowed methods
-//   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-//   optionsSuccessStatus: 200 // Response status for successful OPTIONS requests
-// }));
+app.use(cors({
+  origin: 'https://food-order-khaki.vercel.app', // Frontend URL
+  credentials: true, // Allow credentials like cookies
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  optionsSuccessStatus: 200 // Response status for successful OPTIONS requests
+}));
 
 // app.options('*', cors({
 //   origin: 'https://food-order-khaki.vercel.app', // Frontend URL
@@ -31,27 +31,27 @@ const PORT = process.env.PORT || 8080;
 //   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 //   optionsSuccessStatus: 200 // Response status for successful OPTIONS requests
 // })); // Preflight request handling
-app.use((req, res, next) => {
-  const allowedOrigins = ['https://food-order-khaki.vercel.app', 'http://192.168.1.5:3000'];
-  const origin = req.headers.origin;
-  console.log(origin);
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  }
-  if (req.method === 'OPTIONS') {
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE'
-    );
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization'
-    );
-    return res.status(204).end();
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   const allowedOrigins = ['https://food-order-khaki.vercel.app', 'http://192.168.1.5:3000'];
+//   const origin = req.headers.origin;
+//   console.log(origin);
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader('Access-Control-Allow-Origin', origin);
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//   }
+//   if (req.method === 'OPTIONS') {
+//     res.setHeader(
+//       'Access-Control-Allow-Methods',
+//       'GET,HEAD,PUT,PATCH,POST,DELETE'
+//     );
+//     res.setHeader(
+//       'Access-Control-Allow-Headers',
+//       'Content-Type, Authorization'
+//     );
+//     return res.status(204).end();
+//   }
+//   next();
+// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
