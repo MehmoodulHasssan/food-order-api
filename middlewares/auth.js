a03bb36baee108ae155d033f6b8292b2b9606233
Mehmood Ul Hassan <167098807+MehmoodulHasssan@users.noreply.github.com>
2024-09-04 14:51:51 +0500

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const authForMeals = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
